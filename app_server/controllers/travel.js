51de66b1d4f0f7ddd4ebd279f87a008f3d8ce512
robotsgigantes <sh.carrier@live.com>
2023-12-04 21:41:11 -0500

    Added MVC and Handlebars views

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,8 @@
+/* GET travel view */
+const travel = (req, res) => {
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', { title: pageTitle});
+};
+module.exports = {
+    travel
+};
